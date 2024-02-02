503705b4a02dc077fc2d0bdaae72981da23228f5
GadflyR <140091356+GadflyR@users.noreply.github.com>
2024-02-01 19:00:14 -0500

    simplification1

--- voice_chatbot.py
+++ voice_chatbot.py
@@ -1,43 +1,43 @@
-from telegram.ext import Updater, MessageHandler, Filters
-import telegram, openai, os
-from moviepy.editor import AudioFileClip
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+import telegram, openai
 
 openai.api_key = "***"
 TELEGRAM_API_TOKEN = "***"
-chat_id = '***'
 bot = telegram.Bot(token=TELEGRAM_API_TOKEN)
 messages = [{"role": "system", "content": "You are Q-bot, a helpful AI assistant. "}]
+
+
+def start(update, context):
+    update.message.reply_text(text="Hello! I am Q-bot, your helpful AI assistant.", parse_mode=telegram.ParseMode.MARKDOWN)
 
 
 def tts(text):
     output_file = "temp/output.ogg"
     response = openai.audio.speech.create(model="tts-1", voice="nova", input=text)
     response.stream_to_file(output_file)
-    voice_file = os.path.abspath(output_file)
-    bot.send_voice(chat_id=chat_id, voice=open(voice_file, 'rb'))
+    return output_file
 
 
 def voice_message(update, context):
     voice_file = context.bot.getFile(update.message.voice.file_id)
     voice_file.download("temp/voice_message.ogg")
-    audio_clip = AudioFileClip("temp/voice_message.ogg")
-    audio_clip.write_audiofile("temp/voice_message.mp3")
-    audio_file = open("temp/voice_message.mp3", "rb")
+    audio_file = open("temp/voice_message.ogg", "rb")
     transcript = openai.audio.transcriptions.create(model="whisper-1", file=audio_file).text
     update.message.reply_text(text=f"*[You]:* _{transcript}_", parse_mode=telegram.ParseMode.MARKDOWN)
     messages.append({"role": "user", "content": transcript})
     response = openai.chat.completions.create(model="gpt-3.5-turbo", messages=messages, max_tokens=100)
     ChatGPT_reply = response.choices[0].message.content
-    tts(ChatGPT_reply)
     update.message.reply_text(text=f"*[Q-bot]:* {ChatGPT_reply}", parse_mode=telegram.ParseMode.MARKDOWN)
     if ChatGPT_reply:
         messages.append({"role": "assistant", "content": ChatGPT_reply})
+    with open(tts(ChatGPT_reply), 'rb') as voice_file:
+        update.message.reply_voice(voice=voice_file)
 
 
 def main():
-    bot.send_message(chat_id=chat_id, text="Hello! I am Q-bot, your helpful AI assistant")
     updater = Updater(TELEGRAM_API_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.voice, voice_message))
     updater.start_polling()
     updater.idle()
